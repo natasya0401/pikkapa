730779cc48c577cd732d22fbae7d721eaeb93789
Louis Axel <louisaxel11103@gmail.com>
2023-09-21 09:52:49 +0700

    maps doneee

--- app/src/main/java/com/pikkapa/view/LocationDetailActivity.kt
+++ app/src/main/java/com/pikkapa/view/LocationDetailActivity.kt
@@ -9,13 +9,15 @@ import androidx.databinding.DataBindingUtil
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.MapView
 import com.pikkapa.R
-import com.pikkapa.databinding.ActivityInformationDetailBinding
 import com.pikkapa.databinding.ActivityLocationDetailBinding
 import kotlin.math.log
 
 class LocationDetailActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var mMap: GoogleMap? = null
+//    private var mMap: GoogleMap? = null
+    private lateinit var mapView: MapView
+    private lateinit var mMap: GoogleMap
     private lateinit var binding : ActivityLocationDetailBinding
 
     private var lttd = 51.5074 // Default latitude (e.g., London)
@@ -29,17 +31,26 @@ class LocationDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_location_detail)
 
 
+        binding.toolbar.ivMenu.setOnClickListener{
+            val myIntent = Intent(this, MenuActivity::class.java)
+            this.startActivity(myIntent)
+        }
+
+        binding.toolbar.ivInfo.setOnClickListener {
+            val myIntent = Intent(this, AboutMeActivity::class.java)
+            this.startActivity(myIntent)
+        }
+
         binding.footer.ivBack.setOnClickListener {
             val myIntent = Intent(this, LocationActivity::class.java)
             this.startActivity(myIntent)
         }
-
-        binding.toolbar.tvTitle.text = "LOKASI RS"
-
         binding.footer.ivHome.setOnClickListener {
             val myIntent = Intent(this, HomeActivity::class.java)
             this.startActivity(myIntent)
         }
+
+        binding.toolbar.tvTitle.text = "LOKASI RS"
 
 
         if (intent.hasExtra("title")) {
@@ -66,23 +77,43 @@ class LocationDetailActivity : AppCompatActivity(), OnMapReadyCallback {
             lttd = intent.getStringExtra("latitude").toString().toDouble()
         }
 
+//        val mapFragment = binding.mapView
+//        mapFragment.onCreate(savedInstanceState)
+//        mapFragment.getMapAsync(this)
 
-        val mapFragment = binding.mapView
-        mapFragment.onCreate(savedInstanceState)
-        mapFragment.getMapAsync(this)
-
+//        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+//        mapView = mapFragment.view as MapView
+//        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         val coordinate = LatLng(lgtd, lttd)
-        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 15f))
-        mMap?.uiSettings?.isScrollGesturesEnabled = true
-        mMap?.uiSettings?.isZoomControlsEnabled = true
-        mMap?.uiSettings?.isZoomGesturesEnabled = true
-        mMap?.addMarker(MarkerOptions().position(coordinate).title(title))
-        mMap?.moveCamera(CameraUpdateFactory.newLatLng(coordinate))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 15f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinate))
+
+        mMap.uiSettings.isScrollGesturesEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+
+        mMap.addMarker(MarkerOptions().position(coordinate).title(title))
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
 }
