6a14d0418bd1a9cf380aef15b853d65da26c8f50
Louis Axel <louisaxel11103@gmail.com>
2023-10-12 10:10:19 +0700

    try1

--- app/src/main/java/com/pikkapa/tools/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/pikkapa/tools/alarm/AlarmReceiver.kt
@@ -114,12 +114,18 @@ class AlarmReceiver: BroadcastReceiver() {
         if (context != null) {
             val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
             val intent = Intent(context, AlarmReceiver::class.java)
-            val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                0,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-            )
+//            val pendingIntent = PendingIntent.getBroadcast(
+//                context,
+//                0,
+//                intent,
+//                PendingIntent.FLAG_UPDATE_CURRENT
+//            )
+
+            val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.getBroadcast(context, context.hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
+            } else{
+                PendingIntent.getBroadcast(context, context.hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
 
             // Schedule the next alarm using setExact
             alarmManager.setExact(
